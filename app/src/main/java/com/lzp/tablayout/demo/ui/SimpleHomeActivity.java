b215522e75f818926dd888049e77c1768b5f6b75
dell-PC\Administrator <jcy907394767@163.com>
2020-09-21 11:20:25 +0800

    第一次上传

---
+++ app/src/main/java/com/lzp/tablayout/demo/ui/SimpleHomeActivity.java
@@ -0,0 +1,45 @@
+package com.lzp.tablayout.demo.ui;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.lzp.tablayout.demo.adapter.SimpleHomeAdapter;
+
+public class SimpleHomeActivity extends AppCompatActivity {
+    private Context mContext = this;
+    private final String[] mItems = {
+            "SlidingTabLayout", "CommonTabLayout",
+            "SegmentTabLayout", "SlidingScaleTabLayout(new)",
+            "SlidingScaleTabLayoutFragmentActivity(new)",
+            "SlidingScaleTabLayout without ViewPager(new)"};
+    private final Class<?>[] mClasses = {
+            SlidingTabActivity.class, CommonTabActivity.class,
+            SegmentTabActivity.class, SlidingScaleTabLayoutActivity.class,
+            SlidingScaleTabLayoutFragmentActivity.class,
+            SlidingTabLayoutWithoutViewPagerActivity3.class};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ListView lv = new ListView(mContext);
+        lv.setCacheColorHint(Color.TRANSPARENT);
+        lv.setFadingEdgeLength(0);
+        lv.setAdapter(new SimpleHomeAdapter(mContext, mItems));
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(mContext, mClasses[position]);
+                startActivity(intent);
+            }
+        });
+
+        setContentView(lv);
+    }
+}
